02d0221713287c6aed1db95669bdb9a6a93f47c4
Wakana Kanda <Wakana.Kanda@jp.ricoh.com>
2024-06-19 15:05:23 +0900

    ch08途中

---
+++ ch08/ex12/index.ts
@@ -0,0 +1,11 @@
+// // const f = (functionBody: string) => {
+// //   const arg = ["", "", ""];
+// //   return new Function("$1", "$2", functionBody);
+// // };
+
+// const f = new Function("$1", "$2");
+
+// let arr = [1, 2, 3, 4, 5];
+
+// console.log(arr.reduce(f("$1 + $2"), 0));
+// console.log(arr.sort(f("$1 - $2")));
