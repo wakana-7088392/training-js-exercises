d377391acede73008841ec4caca06f752f9e37da
Wakana Kanda <Wakana.Kanda@jp.ricoh.com>
2024-06-24 12:47:08 +0900

    ch09途中

--- ch09/ex02/index.test.ts
+++ ch09/ex02/index.test.ts
@@ -1,6 +1,6 @@
-import { C } from "./index.js"; // ts でも可
+import { C } from "./index.ts"; // ts でも可
 
-test("", () => {
+test("ch09/ex2", () => {
   const c = new C();
   expect(c.x).toBe(0);
   expect(c.x).toBe(1);
---
+++ ch09/ex02/index.ts
@@ -0,0 +1,6 @@
+export class C {
+  _x = 0;
+  get x() {
+    return this._x++;
+  }
+}
