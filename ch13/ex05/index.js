d703c36fe15fda386db03ef03d94cef2e84b8bdd
Wakana Kanda <Wakana.Kanda@jp.ricoh.com>
2024-08-24 16:15:12 +0900

    ch13/ex05

--- ch13/ex05/index.js
+++ ch13/ex05/index.js
@@ -1,4 +1,4 @@
-import { wait } from "../index.js";
+import { wait, wait3 } from "../index.js";
 
 function g1() {
   // TODO: then のネストを無くしなさい
@@ -13,7 +13,21 @@ function g1() {
   });
 }
 
-function reG1() {}
+// g1のthenのネストを無くした関数
+function reG1() {
+  return wait(1000)
+    .then(() => {
+      console.log("A");
+      return wait(2000);
+    })
+    .then(() => {
+      console.log("B");
+      return wait(3000);
+    })
+    .then(() => {
+      console.log("C");
+    });
+}
 
 function g2() {
   // TODO: new Promise を使わないように書き換えなさい
@@ -26,6 +40,24 @@ function g2() {
       .then(() => console.log("C"))
       .then(resolve, reject);
   });
+}
+
+function reG2() {
+  wait(1000)
+    .then(() => {
+      console.log("A");
+      return wait(2000);
+    })
+    .then(() => {
+      console.log("B");
+      return wait(3000);
+    })
+    .then(() => {
+      console.log("C");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
 
 function g3() {
@@ -52,6 +84,24 @@ function g3() {
     });
 }
 
+function reG3() {
+  function fetchUser() {
+    return Promise.resolve({ id: 42, name: "John" });
+  }
+  function fetchUserFriends(user) {
+    return Promise.resolve([
+      { name: "Sam", id: 100 },
+      { name: "Bob", id: 1 },
+    ]);
+  }
+
+  return fetchUser().then((user) => {
+    return fetchUserFriends(user).then((friends) => {
+      console.log(`${user.name} has ${friends.length} friends!`);
+    });
+  });
+}
+
 function g4() {
   function someFunction() {
     return 42;
@@ -64,4 +114,13 @@ function g4() {
     let value = someFunction();
     return value;
   });
+}
+
+function reG4() {
+  function someFunction() {
+    return 42;
+  }
+
+  const value = someFunction();
+  return value;
 }
---
+++ ch13/ex06/index.md
@@ -0,0 +1,3 @@
+## jQuery Deferred について調べ Promise との関係性について説明しなさい。
+
+#
