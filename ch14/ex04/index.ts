93348df4dc555b3b9fc90b8dab271dbe7826399b
Wakana Kanda <Wakana.Kanda@jp.ricoh.com>
2024-09-11 14:26:02 +0900

    ch14/ex04

---
+++ ch14/ex04/index.ts
@@ -0,0 +1,37 @@
+export class HiraganaUtf16 {
+  hiragana: string;
+  utf16: number;
+
+  constructor(hiragana: string) {
+    // 平仮名一文字であるかを判定する
+    if (!/^[\u3040-\u309F]$/.test(hiragana)) {
+      throw new Error("平仮名一文字のみ有効");
+    }
+
+    this.hiragana = hiragana;
+    this.utf16 = hiragana.charCodeAt(0);
+  }
+  [Symbol.toPrimitive](hint: any) {
+    if (hint === "number") {
+      return this.utf16;
+    } else if (hint === "string") {
+      return this.hiragana;
+    } else {
+      return this.hiragana;
+    }
+  }
+}
+
+export function sortHiragana(arr: HiraganaUtf16[]): HiraganaUtf16[] {
+  return arr.sort((a, b) => (a < b ? -1 : 1));
+}
+
+// const hArr = [
+//   new HiraganaUtf16("あ"),
+//   new HiraganaUtf16("う"),
+//   new HiraganaUtf16("お"),
+//   new HiraganaUtf16("い"),
+//   new HiraganaUtf16("え"),
+// ];
+
+// console.log(sortHiragana(hArr));
