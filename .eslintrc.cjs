13138f90ef590399b6da71eee956765e42df8688
Wakana Kanda <Wakana.Kanda@jp.ricoh.com>
2024-09-22 12:52:59 +0900

    playwrightの設定

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,10 +11,11 @@ module.exports = {
     es2022: true,
     node: true,
     jest: true,
+    browser: true,
   },
   rules: {
     // 本のサンプルコードが基本的にconstを使っていないためerrorからwarnに緩和する。
     // 通常業務ではほぼ確実に採用されるルールのため、offにはしない。
     "prefer-const": "warn",
-  }
+  },
 };
