fa08daa9531a591c5f708d48f6e29d61e0a4fce4
Wakana Kanda <Wakana.Kanda@jp.ricoh.com>
2024-09-27 19:22:16 +0900

    修正

--- ch15.01-03/ex12/index.js
+++ ch15.01-03/ex12/index.js
@@ -1,19 +1,5 @@
 // 無難なもの
 // 入力したキーワードにハイライトをつける
-javascript: (function () {
-  var keyword = prompt("ハイライトするキーワードを入力してください:");
-  if (keyword) {
-    var regex = new RegExp("(" + keyword + ")", "gi");
-    document.body.innerHTML = document.body.innerHTML.replace(
-      regex,
-      '<span style="background-color: yellow;">$1</span>'
-    );
-    alert("キーワードがハイライトされました。");
-  } else {
-    alert("キーワードが入力されていません。");
-  }
-})();
-
 // 選択したら音声が再生される(音声読み上げ)
 javascript: (function () {
   var text = document.body.innerText;
